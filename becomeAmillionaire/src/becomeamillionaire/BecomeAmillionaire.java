a4a45c60e2f956ce4982a9ff94f1fddf5ed6519b
Kevin Bell <bellKevin@pm.me>
2022-05-04 00:38:56 +0000

    Add files via upload

---
+++ becomeAmillionaire/src/becomeamillionaire/BecomeAmillionaire.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package becomeamillionaire;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 4800590195
+ */
+public class BecomeAmillionaire {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
